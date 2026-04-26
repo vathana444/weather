b8db4678577b1051d45534a4a7eb74227a72742f
vathana444 <sokmany15@gmail.com>
2026-04-26 17:50:16 +0700

    Simplify Vite Pages config

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/weather/',
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/index.js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: (assetInfo) =>
-          assetInfo.name?.endsWith('.css')
-            ? 'assets/index.css'
-            : 'assets/[name][extname]',
-      },
-    },
-  },
   plugins: [react()],
 })
